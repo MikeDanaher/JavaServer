b2755d068b55342cbb9f5c8eb77503dbbe7f853b
Mike Danaher <danaher.mike@gmail.com>
2014-09-24 15:44:08 -0500

    Expaned request parser to handle headers and body content

--- src/server/Request.java
+++ src/server/Request.java
@@ -1,13 +1,19 @@
 package server;
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Request {
-    private String fullRequest;
-    private String requestMethod;
-    private String requestURI;
-    private String requestVersion;
+    private List<String>            request;
+    private String                  requestMethod;
+    private String                  requestURI;
+    private String                  requestVersion;
+    private HashMap<String, String> requestHeaders = new HashMap<>();
+    private HashMap<String, String> requestBody = new HashMap<>();
 
     public Request(String fullRequest) {
-        this.fullRequest = fullRequest;
+        request = Arrays.asList(fullRequest.split("\r\n"));
     }
 
     public String getMethod() {
@@ -22,16 +28,64 @@ public class Request {
         return requestVersion;
     }
 
+    public HashMap<String, String> getHeaders() {
+        return requestHeaders;
+    }
+
+    public HashMap<String, String> getBody() {
+        return requestBody;
+    }
+
     public Request parseFullRequest() {
-        String[] request = fullRequest.split("\r\n");
-        parseRequestLine(request[0]);
+        int indexOfBlankLine = request.indexOf("");
+        parseRequestLine(0);
+        parseHeaders(1, indexOfBlankLine);
+        if (indexOfBlankLine != -1) {
+            parseBody(indexOfBlankLine + 1);
+        }
         return this;
     }
 
-    private void parseRequestLine(String requestLine) {
-        String[] rl = requestLine.split(" ");
-        requestMethod = rl[0];
-        requestURI = rl[1];
-        requestVersion = rl[2];
+    private void parseRequestLine(int firstIndex) {
+        String[] requestLine = request.get(firstIndex).split(" ");
+        requestMethod = requestLine[0];
+        requestURI = requestLine[1];
+        requestVersion = requestLine[2];
+    }
+
+    private void parseHeaders(int start, int end) {
+        for (int i = start; i < end; i++) {
+            String[] headerLine = request.get(i).split(" ");
+            String key = headerLine[0];
+            String value = headerLine[1];
+            requestHeaders.put(key, value);
+        }
+    }
+
+    private void parseBody(int dataIndex) {
+        if (isPatch()) {
+            parseBodyContent(dataIndex);
+        } else {
+            parseURLEncodedData(dataIndex);
+        }
+    }
+
+    private void parseURLEncodedData(int dataIndex) {
+        String[] data = request.get(dataIndex).split("&");
+
+        for (String dataPoint : data) {
+            String[] params = dataPoint.split("=");
+            String key = params[0];
+            String value = params[1];
+            requestBody.put(key, value);
+        }
+    }
+
+    private void parseBodyContent(int dataIndex) {
+        requestBody.put("Content", request.get(dataIndex));
+    }
+
+    private boolean isPatch() {
+        return requestMethod.equals("PATCH");
     }
 }
--- test/server/RequestTest.java
+++ test/server/RequestTest.java
@@ -2,32 +2,83 @@ package server;
 
 import org.junit.Before;
 import org.junit.Test;
-
+import java.util.HashMap;
 import static org.junit.Assert.assertEquals;
 
 public class RequestTest {
 
-    private String FULL_GET_REQUEST = "GET /test HTTP/1.1";
-    private Request request;
+    private Request POSTRequest;
+    private Request GETRequest;
+    private Request PATCHRequest;
+    private String POST_URLENCODED = "POST /test HTTP/1.1\r\n" +
+                                     "Host: localhost:5000\r\n" +
+                                     "Connection: keep-alive\r\n" +
+                                     "Content-Type: application/x-www-form-urlencoded\r\n" +
+                                     "\r\n" +
+                                     "name=Test&email=test@example.com";
+
+    private String PATCH_REQUEST = "PATCH /test.txt HTTP/1.1\r\n" +
+                                   "Host: localhost:5000\r\n" +
+                                   "Connection: keep-alive\r\n" +
+                                   "Content-Type: application/x-www-form-urlencoded\r\n" +
+                                   "\r\n" +
+                                   "This is some new content";
+
+    private String GET_REQUEST = "POST /test HTTP/1.1\r\n" +
+                                 "Host: localhost:5000\r\n" +
+                                 "Connection: keep-alive\r\n" +
+                                 "Content-Type: text/html\r\n" +
+                                 "\r\n";
 
     @Before
     public void createRequest() {
-        request = new Request(FULL_GET_REQUEST).parseFullRequest();
+        POSTRequest = new Request(POST_URLENCODED).parseFullRequest();
     }
 
     @Test
     public void testGetMethod() {
-        assertEquals(request.getMethod(), "GET");
+        assertEquals(POSTRequest.getMethod(), "POST");
     }
 
     @Test
     public void testGetURI() {
-        assertEquals(request.getURI(), "/test");
+        assertEquals(POSTRequest.getURI(), "/test");
     }
 
     @Test
     public void testGetVersion() {
-        assertEquals(request.getVersion(), "HTTP/1.1");
+        assertEquals(POSTRequest.getVersion(), "HTTP/1.1");
     }
 
+    @Test
+    public void testGetHeaders() {
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put("Host:", "localhost:5000");
+        headers.put("Connection:", "keep-alive");
+        headers.put("Content-Type:", "application/x-www-form-urlencoded");
+        assertEquals(POSTRequest.getHeaders(), headers);
+    }
+
+    @Test
+    public void testGetPostData() {
+        HashMap<String, String> body = new HashMap<>();
+        body.put("name", "Test");
+        body.put("email", "test@example.com");
+        assertEquals(POSTRequest.getBody(), body);
+    }
+
+    @Test
+    public void testEmptyBody() {
+        GETRequest = new Request(GET_REQUEST).parseFullRequest();
+        HashMap<String, String> body = new HashMap<>();
+        assertEquals(GETRequest.getBody(), body);
+    }
+
+    @Test
+    public void testGetPatchContent() {
+        PATCHRequest = new Request(PATCH_REQUEST).parseFullRequest();
+        HashMap<String, String> body = new HashMap<>();
+        body.put("Content", "This is some new content");
+        assertEquals(PATCHRequest.getBody(), body);
+    }
 }
